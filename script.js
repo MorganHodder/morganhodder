9a4b23558ed910725b09f27ae9a72532da0f8d9b
MorganHodder <91983158+MorganHodder@users.noreply.github.com>
2022-09-09 20:28:57 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+////////////////////////
+//ON BUTTON CLICK OPEN//
+////////////////////////
+function vklassOpen() {
+    window.open("https://auth.vklass.se/saml/initiate?idp=http://fs.lund.se/adfs/services/trust&org=60");
+  }
+
+function schemaOpen() {
+    window.open("https://web.skola24.se/timetable/timetable-viewer/lund.skola24.se/Polhemskolan/")
+}
+
+function lunchOpen() {
+    window.open("https://skolmaten.se/polhemsskolan2/")
+}
