610b646917805727cd2a5a2af44dfbb507bc5a92
MorganHodder <91983158+MorganHodder@users.noreply.github.com>
2022-09-24 15:44:36 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 ////////////////////////
 //ON BUTTON CLICK OPEN//
 ////////////////////////
+
 function vklassWindow() {
     window.open("https://auth.vklass.se/saml/initiate?idp=http://fs.lund.se/adfs/services/trust&org=60");
 }
@@ -13,6 +14,14 @@ function toggleSchema(){
     return openSchema();
 }
 
+function toggleLunch(){
+    iframeClose = document.getElementById("lunchIframe").style.display;
+    if (iframeClose == "block") {
+        return closeLunch();
+    }
+    return openLunch();
+}
+
 function openSchema() {
     document.getElementById("schemaIframe")
         .style.display = "block";
@@ -23,16 +32,37 @@ function closeSchema() {
         .style.display = "none";
 }
 
-function lunchWindow() {
-    window.open("https://skolmaten.se/polhemsskolan2/")
+function openLunch() {
+    document.getElementById("lunchIframe")
+        .style.display = "block";
 }
 
-var element_pos = 0;    // Position of the newly created elements.
-var iCnt = 0;
+function closeLunch() {
+     document.getElementById("lunchIframe")
+        .style.display = "none";
+}
 
 $(document).ready(function() {
 
     $(function() { $("#schemaIframe").draggable({ containment: "window" }); });
 
-        iCnt = iCnt + 1;
 });
+
+$(document).ready(function() {
+
+    $(function() { $("#lunchIframe").draggable({ containment: "window" }); });
+
+});
+
+/////////////////////
+///GET WEEK NUMBER///
+/////////////////////
+
+currentDate = new Date();
+    startDate = new Date(currentDate.getFullYear(), 0, 1);
+    var days = Math.floor((currentDate - startDate) /
+        (24 * 60 * 60 * 1000));
+         
+    var weekNumber = Math.ceil(days / 7);
+    
+    document.getElementById("weekNmbr").innerHTML = "Vecka " + weekNumber;
